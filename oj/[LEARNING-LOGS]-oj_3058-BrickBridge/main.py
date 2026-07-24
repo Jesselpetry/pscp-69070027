a725c0194e11a9bcea276a7b1061ae3e862d509f
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2026-07-24 16:17:00 +0700

    Update main.py

--- oj/[LEARNING-LOGS]-oj_3058-BrickBridge/main.py
+++ oj/[LEARNING-LOGS]-oj_3058-BrickBridge/main.py
@@ -2,6 +2,18 @@
 
 def main():
     """BrickBridge"""
+    a = int(input(""))
+    b = int(input(""))
+    goal = int(input(""))
+    big_brick = b * 5
+    all_brick = big_brick + a
+    remainder = 0
+    if all_brick >= goal:
+        remainder = goal % 5
+        if remainder <= a:
+            print(remainder)
+    else:
+        print(-1)
 
 if __name__ == "__main__":
     main()
