acf221249c97bfefcdf9681a46cebb21b9baf5aa
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2026-07-30 21:41:08 +0700

    Update main.py

--- oj3042/main.py
+++ oj3042/main.py
@@ -2,6 +2,16 @@
 
 def main():
     """หาร 10"""
+    n = int(input())
+    ans = []
+    num = n - (n % 10)
+
+    if not num % 10:
+        rounds = num // 10
+        for _ in range(rounds+1):
+            ans.append(num)
+            num = num-10
+    print(*ans)
 
 if __name__ == "__main__":
     main()
