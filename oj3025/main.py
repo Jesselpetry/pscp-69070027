a27665c398d387efcda9d30849a17e9b7da4d212
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2026-07-30 17:29:16 +0700

    refactor: remove submission template andd add oj3025 main.py

--- oj3025/main.py
+++ oj3025/main.py
@@ -2,7 +2,22 @@
 
 def main():
     """Season"""
-    # เขียนโค้ดที่นี่
+    month = int(input())
+    day = int(input())
+
+    winter = [1,2]
+    spring = [4,5]
+    summer = [7,8]
+    fall = [10,11]
+
+    if (month in winter) or (month == 12 and day >= 21) or (month == 3 and day < 21):
+        print("winter")
+    elif (month in spring) or (month == 3 and day >= 21) or (month == 6 and day < 21):
+        print("spring")
+    elif (month in summer) or (month == 6 and day >= 21) or (month == 9 and day < 21):
+        print("summer")
+    elif (month in fall) or (month == 9 and day >= 21) or (month == 12 and day < 21):
+        print("fall")
 
 if __name__ == "__main__":
     main()
