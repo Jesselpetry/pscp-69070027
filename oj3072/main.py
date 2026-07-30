f835bcdf37a2c921634f58b8effc17d83596c2eb
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2026-07-30 19:10:24 +0700

    Update main.py

--- oj3072/main.py
+++ oj3072/main.py
@@ -2,6 +2,35 @@
 
 def main():
     """A-E-I-O-U"""
+    text = str(input().lower())
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    a = 0
+    e = 0
+    i = 0
+    o = 0
+    u = 0
+    for char in text:
+        if char in vowels:
+            if char == 'a':
+                a = a + 1
+            elif char == 'e':
+                e = e + 1
+            elif char == 'i':
+                i = i + 1
+            elif char == 'o':
+                o = o + 1
+            elif char == 'u':
+                u = u + 1
+    if a > 0:
+        print(f"a : {a}")
+    if e > 0:
+        print(f"e : {e}")
+    if i > 0:
+        print(f"i : {i}")
+    if o > 0:
+        print(f"o : {o}")
+    if u > 0:
+        print(f"u : {u}")
 
 if __name__ == "__main__":
     main()
