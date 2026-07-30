84d8c913b9727db3ec316337ab0b815ab6faa4ac
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2026-07-30 21:18:55 +0700

    Update main.py

--- oj3058/main.py
+++ oj3058/main.py
@@ -2,16 +2,16 @@
 
 def main():
     """BrickBridge"""
-    a = int(input(""))
-    b = int(input(""))
-    goal = int(input(""))
-    big_brick = b * 5
-    all_brick = big_brick + a
-    remainder = 0
-    if all_brick >= goal:
-        remainder = goal % 5
-        if remainder <= a:
-            print(remainder)
+    a = int(input())
+    b = int(input())
+    goal = int(input())
+
+    used_big = min(goal // 5, b)
+
+    needed_small = goal - (used_big * 5)
+
+    if needed_small <= a:
+        print(needed_small)
     else:
         print(-1)
 
