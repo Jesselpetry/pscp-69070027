262351f8be1fe9606df10ef50cf3899caa135673
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2026-07-30 17:00:17 +0700

    Update main.py

--- oj3017/main.py
+++ oj3017/main.py
@@ -2,8 +2,15 @@
 
 def main():
     """[LEARNING LOGS] Bill"""
-    # คุณแค่มีหน้าที่เอาโค้ดมาแปะหรือพิมพ์ต่อตรงนี้ได้เลย
-    pass
+    subtotal = float(input())
+    service_charge = subtotal / 10
+    if service_charge < 50:
+        service_charge = 50
+    elif service_charge > 1000:
+        service_charge = 1000
+    vat = (subtotal + service_charge) * 0.07
+    total = subtotal + service_charge + vat
+    print(f"{total:.2f}")
 
 if __name__ == "__main__":
     main()
