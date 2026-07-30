878568015607ee5afee2f1e849900de206047114
Chatan Petry <95079984+Jesselpetry@users.noreply.github.com>
2026-07-30 18:24:08 +0700

    Update main.py

--- oj3024/main.py
+++ oj3024/main.py
@@ -2,7 +2,16 @@
 
 def main():
     """SurprisingVote"""
-    # เขียนโค้ดที่นี่
+    vote = float(input())
+    highest_vote = float(input())
+    lowest_vote = vote - (2 * highest_vote)
+    if lowest_vote < 0:
+        lowest_vote = 0
+
+    if highest_vote - lowest_vote > 2:
+        print("Surprising")
+    else:
+        print("Not surprising")
 
 if __name__ == "__main__":
     main()
